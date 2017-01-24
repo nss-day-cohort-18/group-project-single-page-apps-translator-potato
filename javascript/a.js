c39cc3f41ad819766b4b0733d91e341f6d04f497
Adam Myers <Tavern@Adams-Mac.local>
2017-01-24 14:49:27 -0600

    Work in progress

--- javascript/a.js
+++ javascript/a.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var translateButton = document.getElementById("translate-button");
+var languageSelected = document.getElementById("language-selected");
 
 var Translate = (function(){
 	var russian = {
@@ -20,6 +21,22 @@ var Translate = (function(){
 
 })();
 
+
 translateButton.addEventListener('click', function(){
-	
+	var inputField = document.getElementById("input-field").value;
+	var outputField = document.getElementById("translated");
+	var outputText;
+
+	if (languageSelected.value === "russian") {
+		outputText = Translate.toRussian(inputField);
+	} else if (languageSelected.value === "spanish") {
+		outputText = Translate.toSpanish(inputField);
+	} else if (languageSelected.value === "elvish") {
+		outputText = Translate.toElvish(inputField);
+	} else {
+		alert("Please select a language")
+	}
+
+	outputField.innerHTML = outputText;
+
 })
