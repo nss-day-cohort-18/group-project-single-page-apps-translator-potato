1365a8e91e88ed4805b782b91de65266bbff573d
Dean Smith <Dean Smith>
2017-01-25 09:49:29 -0600

    added more words to elvish

--- javascript/d.js
+++ javascript/d.js
@@ -8,6 +8,25 @@ var Translate = (function(originalTranslate){
 		happy: "happui",
 		new: "siniath",
 		year: "ín",
+		one: "er",
+		does: "does",
+		not: "ú",
+		simply: " simplui",
+		walk: "padad",
+		into: "into",
+		mordor: "Mordor"
+		they: "hain",
+		may: "lothron",
+		take: "take",
+		our: "mín",
+		lives: "lives",
+		but: "but",
+		//they
+		will: "will",
+		take: "take",
+		never: "never",
+		our: "mín",
+		freedom: "freedom"
 	};
 
 	originalTranslate.toElvish = function(potato){
